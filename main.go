2fa107fcb6523656665bec5f49bf67dc90105c8e
Olivér Falvai <oliver.falvai@bitrise.io>
2023-02-09 11:37:22 +0100

    Remove beta label and other cleanups

--- main.go
+++ main.go
@@ -6,28 +6,27 @@ import (
 	"github.com/bitrise-io/go-steputils/v2/stepconf"
 	"github.com/bitrise-io/go-utils/v2/command"
 	"github.com/bitrise-io/go-utils/v2/env"
+	. "github.com/bitrise-io/go-utils/v2/exitcode"
 	"github.com/bitrise-io/go-utils/v2/log"
 	"github.com/bitrise-steplib/steps-restore-spm-cache/step"
 )
 
 func main() {
-	os.Exit(run())
+	exitCode := run()
+	os.Exit(int(exitCode))
 }
 
-func run() int {
+func run() ExitCode {
 	logger := log.NewLogger()
 	envRepo := env.NewRepository()
 	inputParser := stepconf.NewInputParser(envRepo)
 	cmdFactory := command.NewFactory(envRepo)
 	cacheStep := step.New(logger, inputParser, envRepo, cmdFactory)
 
-	exitCode := 0
-
 	if err := cacheStep.Run(); err != nil {
 		logger.Errorf(err.Error())
-		exitCode = 1
-		return exitCode
+		return Failure
 	}
 
-	return exitCode
+	return Success
 }
